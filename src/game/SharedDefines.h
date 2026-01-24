c41ffec7803972d849300244b8caf5e1fc937302
Paco1967 <157508103+Paco1967@users.noreply.github.com>
2026-01-24 08:26:15 -0500

    Adding Goblins as neutral playable race.

--- src/game/SharedDefines.h
+++ src/game/SharedDefines.h
@@ -67,12 +67,12 @@ enum Races
 };
 
 // max+1 for player race
-#define MAX_RACES         9
+#define MAX_RACES         10
 
 #define RACEMASK_ALL_PLAYABLE \
     ((1<<(RACE_HUMAN-1))    |(1<<(RACE_ORC-1))      |(1<<(RACE_DWARF-1))   | \
     (1<<(RACE_NIGHTELF-1))  |(1<<(RACE_UNDEAD-1))   |(1<<(RACE_TAUREN-1))  | \
-    (1<<(RACE_GNOME-1))     |(1<<(RACE_TROLL-1)))
+    (1<<(RACE_GNOME-1))     |(1<<(RACE_TROLL-1))    |(1<<(RACE_GOBLIN-1)))
 
 // for most cases batter use ChrRace data for team check as more safe, but when need full mask of team can be use this defines.
 #define RACEMASK_ALLIANCE \
@@ -82,6 +82,9 @@ enum Races
 #define RACEMASK_HORDE \
     ((1<<(RACE_ORC-1))      |(1<<(RACE_UNDEAD-1))   |(1<<(RACE_TAUREN-1))  | \
     (1<<(RACE_TROLL-1)))
+
+#define RACEMASK_NEUTRAL \
+    ((1<<(RACE_GOBLIN-1)))
 
 // Class value is index in ChrClasses.dbc
 enum Classes
