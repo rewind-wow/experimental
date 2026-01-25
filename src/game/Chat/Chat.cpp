43f47165abd60433441436c7204a6f53e09062a6
Paco1967 <157508103+Paco1967@users.noreply.github.com>
2026-01-25 15:07:49 -0500

    Chat.cpp modified for Goblins

--- src/game/Chat/Chat.cpp
+++ src/game/Chat/Chat.cpp
@@ -3856,6 +3856,7 @@ static RaceMaskName const raceMaskNames[] =
     { "tauren", (1 << (RACE_TAUREN - 1))  },
     { "gnome", (1 << (RACE_GNOME - 1))   },
     { "troll", (1 << (RACE_TROLL - 1))   },
+    { "goblin", (1 << (RACE_GOBLIN - 1)) },
 
     // masks
     { "alliance", RACEMASK_ALLIANCE },
