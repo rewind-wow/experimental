ca968ebc6a19a635d681e0bc7b7f3318fdac048e
Paco1967 <157508103+Paco1967@users.noreply.github.com>
2026-01-31 13:02:47 -0500

    Player.cpp Race reputation reset to stock

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -21329,8 +21329,6 @@ uint32 GetCapitalReputationForRace(uint8 race)
             return 68; // Undercity
         case RACE_TAUREN:
             return 81; // Thunder Bluff
-        case RACE_GOBLIN:    
-            return 169; // Steamwheedle Cartel
         default:
             return 0;
     }
