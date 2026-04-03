fb655ef9532fc1a1d45d9b8931b183a1f3b74445
Paco1967 <157508103+Paco1967@users.noreply.github.com>
2026-04-02 21:20:15 -0400

    Add goblins to game

--- src/game/Objects/Creature.cpp
+++ src/game/Objects/Creature.cpp
@@ -1393,6 +1393,9 @@ bool Creature::IsTrainerOf(Player* pPlayer, bool msg) const
                         case RACE_UNDEAD:
                             pPlayer->PlayerTalkClass->SendGossipMenu(624, GetObjectGuid());
                             break;
+                         case RACE_GOBLIN:
+                            pPlayer->PlayerTalkClass->SendGossipMenu(5863, GetObjectGuid());
+                            break;
                     }
                 }
                 return false;
